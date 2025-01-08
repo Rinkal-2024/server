cde27fe75980e40fb0188f7b0c4f17d23a74ff0a
Rinkal Dhankariya <rinkal.dhankariya@iflair.com>
2025-01-08 19:41:39 +0530

    file added

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const userRoutes = require('./routes/user');
 const app = express();
 
 app.use(cors({
-  origin : 'https://news-website-demo-ten.vercel.app'
+  origin : 'https://news-website-demo-ten.vercel.app',
+  credentials: include
 }
 ));
 app.use(express.json()); 
